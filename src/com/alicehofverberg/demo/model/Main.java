7dbf1e016b3ee427b7f95a94ed9ebe15a27901e5
aliceh0155 <hofverbergalice@gmail.com>
2023-09-20 20:03:01 +0200

    Fist Commit

---
+++ src/com/alicehofverberg/demo/model/Main.java
@@ -0,0 +1,53 @@
+package com.alicehofverberg.demo.model;
+
+public class Main {
+
+    public static int doubleMyAge (int age){
+
+        return age*2;
+    }
+    public static void main(String[] args) {
+
+        //instantiate object
+        Student benny = new Student("Benny",15,true);
+
+        System.out.println(benny.name + benny.age);
+
+
+        int sum = 0;
+       sum = doubleMyAge(15);
+        System.out.println(sum);
+
+       /* benny.name = "Benny";
+        frida.name = "Frida";
+        benny.age = 15;
+        frida.age = 22;
+
+        System.out.println(benny.name);
+        System.out.println(benny.age);
+        System.out.println(frida.name);
+        System.out.println(frida.age);
+         */
+
+
+
+        /*
+        int sum = 0;
+        int sum2 = 0;
+
+        sum = addition(15,25); //this returns
+        sum2 = addition2(15,15); //this does not return
+    }
+
+    public static int addition (int x, int y){
+
+
+        return x+y;
+    }
+
+    public static void addition2 (int x , int y){
+        System.out.println(x+y);
+*/
+    }
+
+}
---
+++ src/com/alicehofverberg/demo/model/Student.java
@@ -0,0 +1,17 @@
+package com.alicehofverberg.demo.model;
+
+public class Student {
+
+    public String name;
+    public int age;
+
+    public boolean isTired;
+
+
+    public Student(String name, int age, boolean isTired) {
+        this.name = name;
+        this.age = age;
+        this.isTired = isTired;
+
+    }
+}
